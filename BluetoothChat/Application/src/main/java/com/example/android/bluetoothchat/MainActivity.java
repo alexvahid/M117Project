e92a96cdda28d363a28e9e9bc512d8ca9c6b119d
yoel cohavy <yoelcohavy@Evil-2.local>
2018-03-08 13:30:39 -0800

    removed the SHOW LOG thing

--- BluetoothChat/Application/src/main/java/com/example/android/bluetoothchat/MainActivity.java
+++ BluetoothChat/Application/src/main/java/com/example/android/bluetoothchat/MainActivity.java
@@ -71,9 +71,9 @@ public class MainActivity extends SampleActivityBase {
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        MenuItem logToggle = menu.findItem(R.id.menu_toggle_log);
+        /*MenuItem logToggle = menu.findItem(R.id.menu_toggle_log);
         //logToggle.setVisible(findViewById(R.id.sample_output) instanceof ViewAnimator);
-        logToggle.setTitle(mLogShown ? R.string.sample_hide_log : R.string.sample_show_log);
+        logToggle.setTitle(mLogShown ? R.string.sample_hide_log : R.string.sample_show_log);*/
 
         return super.onPrepareOptionsMenu(menu);
     }
